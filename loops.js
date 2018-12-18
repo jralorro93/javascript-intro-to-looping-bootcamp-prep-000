3d8b586b0394b2293e7074f7b21bd85b2e716abe
Juan Alorro <jralorro93@gmail.com>
2018-12-18 21:18:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -6,6 +6,16 @@ function forLoop(array) {
 		else {
 			return `I am ${i} strange loops.`;
 		}
-	return array;
 	}
+	return array;
+}
+function forLoop(array) {
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push('I am 1 strange loop.')
+    } else {
+      array.push(`I am ${i} strange loops.`)
+  }
+}
+return array
 }
